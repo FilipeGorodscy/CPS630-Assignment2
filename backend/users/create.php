17c5e8a2aff6012e4d74b6fede380ecda195dacb
Seit Sorra <seitsorra1994@gmail.com>
2021-04-13 19:26:02 -0400

    wip

---
+++ backend/users/create.php
@@ -0,0 +1,42 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../config/database.php';
+include_once '../tables/users.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$user = new User($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+if(!empty($data->username) && !empty($data->password)
+    && !empty($data->email) && !empty($data->phone) && !empty($data->address) ){
+
+    $user->username = $data->username; 
+    $user->password = md5($data->password);
+    $user->email = $data->email;
+    $user->phone = $data->phone;
+    $user->address = $data->address;
+
+    if($user->create()){
+        http_response_code(200);
+        echo json_encode(array("message" => "User was created successfully."));
+    }
+    else{
+        http_response_code(503); // unavailable service code
+        echo json_encode(array("message" => "Unable to create user."));
+    }
+}
+else{
+    http_response_code(400); // Bad request code
+    echo json_encode(array("message" => "Missing mandatory fields"));
+}
+
+?>
---
+++ backend/users/read_one.php
@@ -0,0 +1,59 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+  
+include_once '../config/database.php';
+include_once '../tables/users.php';
+  
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+  
+$user = new User($db);
+
+$found = false;
+
+$data = json_decode(file_get_contents("php://input"));
+
+// Check if user_id was requested 
+if(!empty($data->id)){
+    $user->id = $data->id;
+    if($user->readOne()){
+        $found = true;
+    }
+}
+// Check if username was requested
+else if(!empty($data->username) && !empty($data->password) ){
+    $user->username = $data->username;
+    $user->password = md5($data->password);
+
+    if($user->username_exists()){
+        $found = true;
+    }
+}
+
+if($found){
+    $user_array=array(
+        "id" => $user->id,
+        "username" => $user->username,
+        "password" => $user->password,
+        "email" => $user->email,
+        "phone" => $user->phone,
+        "address" => $user->address,
+        "created_at" => $user->created_at
+    );
+
+    // set response code - 200 OK
+    http_response_code(200);
+    echo json_encode($user_array);
+}
+else{
+    // set response code - 404 Not found
+    http_response_code(404);
+    echo json_encode(array("message" => "Unable to find requested user."));
+}
+?>
