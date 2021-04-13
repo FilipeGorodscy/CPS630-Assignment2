3a1b535acf3a8cf080461974b72b805db3f01729
RumGuru <Rahul.kothwal@hotmail.com>
2021-04-13 18:07:31 -0400

    Update package-lock.json

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import axios from "axios";
 
-const Delivery = () => {
+const Delivery = ({total, setTotal}) => {
+
+const [flowers, setFlowers] = useState([]);
+
+useEffect(() => {
+    const fetchFlowers = async () => {
+      const res = await axios.get("http://localhost/backend/flowers/read.php");
+      const flowers = res.data.records;
+      setFlowers(flowers);
+    };
+    fetchFlowers();
+  }, []);
+  
   return <div>Delivery service</div>;
+
 };
 
 export default Delivery;
+
+
+/*const Ride = ({ total, setTotal }) => {
+  const [source, setSource] = useState();
+  const [destination, setDestination] = useState();
+  const [distance, setDistance] = useState();
+  const [cars, setCars] = useState([]);*/
+
+// Flower object contains 
