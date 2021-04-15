14dbb0233a06c3c06ca7a359f0ce85ec1c569645
RumGuru <Rahul.kothwal@hotmail.com>
2021-04-15 10:42:01 -0400

    Update OrderItem.js

--- src/components/Checkout-ShoppingCart/OrderItem.js
+++ src/components/Checkout-ShoppingCart/OrderItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-const OrderItem = ({ flowerName, date, source, destination, carName, carModel, price }) => {
+const OrderItem = ({ flowerName, groceryName, date, source, destination, carName, carModel, price }) => {
   return (
     <Card>
       <Card.Header className="bg-dark text-light">Date: {date}</Card.Header>
@@ -9,10 +9,12 @@ const OrderItem = ({ flowerName, date, source, destination, carName, carModel, p
         <Card.Title>
           {source && `${source} to ${destination}`}
           {flowerName && "Delivery Service"}
+          {GroceryName && "Delivery Service"}
         </Card.Title>
         <Card.Text>
           Car: {carName} {carModel} <br />
           {flowerName && `Flower: ${flowerName}`}
+          {groceryName && `Item: ${groceryName}`}
         </Card.Text>
         <Card.Footer>Price: ${price}</Card.Footer>
       </Card.Body>
